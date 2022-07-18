44b44740c82ba3d79147f627959e9036d0f725c4
TheJim123 <39704643+TheJim123@users.noreply.github.com>
2022-07-18 23:14:32 +0200

    Narisal skatle z brki za 1a)

---
+++ Skatle.R
@@ -0,0 +1,19 @@
+#Nastavite lokacijo na STAT
+setwd("C:/Users/Uporabnik/Documents/GitHub/STAT")
+
+#Vzorec "KibergradVzorec1.csv" je bil pridobljen preko vzorčenja v LibreOffice Calc
+
+#Pretvorimo tabelo v obliki csv datoteke v dataframe, s katerimi R operira
+
+vzorec1 <- read.csv("KibergradVzorec1.csv")
+
+#Shranimo si vnose za tipe in za dohodke
+
+Tip <- vzorec1$TIP
+Dohodek <- vzorec1$DOHODEK
+
+#Narišemo vzporedne škatle z brki
+
+boxplot(Dohodek~Tip, data = vzorec1, main="Škatle z brki za dohodke družin glede na tip",yaxp = c(0, 250000, 10), xlab = "Tip Družine", ylab = "Dohodek")
+
+
