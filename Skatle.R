d38b8c412608607528a05b040f024efb5bac988d
Jimmy <deejay.jimmy.1@gmail.com>
2022-08-08 20:18:20 +0200

    Naloga 1b WIP
    Posebej shranil vzorce za nalogo 1b, narisal skatlo z brki.

--- Skatle.R
+++ Skatle.R
@@ -1,6 +1,6 @@
 #Nastavite lokacijo na STAT
-setwd("C:/Users/Uporabnik/Documents/GitHub/STAT")
-
+#setwd("C:/Users/Uporabnik/Documents/GitHub/STAT")
+setwd("C:\Users\Jimmy\Desktop\faks\3. letnik\STAT\Projektna-STAT")
 #Vzorec "KibergradVzorec1.csv" je bil pridobljen preko vzorčenja v LibreOffice Calc
 
 #Pretvorimo tabelo v obliki csv datoteke v dataframe, s katerimi R operira
@@ -15,5 +15,3 @@ Dohodek <- vzorec1$DOHODEK
 #Narišemo vzporedne škatle z brki
 
 boxplot(Dohodek~Tip, data = vzorec1, main="Škatle z brki za dohodke družin glede na tip",yaxp = c(0, 250000, 10), xlab = "Tip Družine", ylab = "Dohodek")
-
-
